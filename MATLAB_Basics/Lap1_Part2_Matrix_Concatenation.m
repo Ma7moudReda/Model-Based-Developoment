4b6045afe1b93d5efdfc6706e986caaecb73fdcd
Mahmoud Reda <47426350+Ma7moudReda@users.noreply.github.com>
2023-10-12 20:25:56 +0300

    Update Lap1_Part2_Matrix_Concatenation.m

--- MATLAB_Basics/Lap1_Part2_Matrix_Concatenation.m
+++ MATLAB_Basics/Lap1_Part2_Matrix_Concatenation.m
@@ -2,5 +2,5 @@ clear
 clc
 rowVector = [1 2 3 4 5];
 columnVector = [6;7;8;9;10];
-horizontalConcat_1 = cat(1,rowVector,columnVector) %method 1
-horizontalConcat_2 = [rowVector,columnVector]      %method 2
+horizontalConcat_1 = cat(1,rowVector,columnVector') %method 1
+horizontalConcat_2 = [rowVector,columnVector']      %method 2
